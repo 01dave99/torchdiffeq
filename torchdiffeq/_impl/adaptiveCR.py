Fixed worng estimator calculation

torchdiffeq/_impl/adaptiveCR.py
@@ -9,6 +9,8 @@ class AdaptiveCRSolver(AdaptiveGridODESolver):
 
     def _step_func(self, func, t0, dt, t1, y0):
         y0_np=y0.detach().cpu().numpy().reshape(-1)
+        t0_np=t0.detach().cpu().numpy().reshape(-1)
+        t1_np=t1.detach().cpu().numpy().reshape(-1)
         def np_func(t, y):
             t = torch.tensor(t).to(y0.device, y0.dtype)
             y = torch.reshape(torch.tensor(y).to(y0.device, y0.dtype), y0.shape)
@@ -16,8 +18,9 @@ class AdaptiveCRSolver(AdaptiveGridODESolver):
                 f = func(t, y)
             return f.detach().cpu().numpy().reshape(-1)
         def optim_f(y):
-            return y-y0_np-(t1.detach().cpu().numpy().reshape(-1)-t0.detach().cpu().numpy().reshape(-1))*(np_func(t1.detach().cpu().numpy().reshape(-1),y)+np_func(t0.detach().cpu().numpy().reshape(-1),y0_np))/2
-        root=sci.optimize.root(optim_f,y0_np)
+            return y-y0_np-(t1_np-t0_np)*(np_func(t1_np,y)+np_func(t0_np,y0_np))/2
+        root=sci.optimize.root(optim_f,y0_np,options={'xtol':1e-10})
+        #print(root['success'])
         tr=torch.reshape(torch.tensor(root["x"]).to(y0.device, y0.dtype), y0.shape)
         return tr, func(t0,y0)
     
@@ -35,8 +38,8 @@ class AdaptiveCRSolver(AdaptiveGridODESolver):
                 f = func(t, yfull)[range(len(y0)-num_paras)]
             return f.detach().cpu().numpy().reshape(-1)
         def optim_f(y):
-            return y-y0_np-(t1.detach().cpu().numpy().reshape(-1)-t0.detach().cpu().numpy().reshape(-1))*(np_func(t1.detach().cpu().numpy().reshape(-1),y)+np_func(t0.detach().cpu().numpy().reshape(-1),y0_np))/2
-        root=sci.optimize.root(optim_f,y0_np)
+            return y-y0_np-(t1_np-t0_np)*(np_func(t1_np,y)+np_func(t0_np,y0_np))/2
+        root=sci.optimize.root(optim_f,y0_np,tol=1e-14)
         tr=torch.reshape(torch.tensor(root["x"]).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape)
         #restlichen variablen:
         frest0=func(t0,y0)[range(-num_paras,0)]
@@ -50,29 +53,24 @@ class AdaptiveCRSolver(AdaptiveGridODESolver):
         y1_np=y1.detach().cpu().numpy().reshape(-1)
         t0_np=t0.detach().cpu().numpy().reshape(-1)
         t1_np=t1.detach().cpu().numpy().reshape(-1)
-        def dtfunc(t,y):
-            def tfunc(t):
-                return self.func(t,y)
-            
-            return torch.autograd.functional.jacobian(tfunc,t)
+        
+       
         def np_dtfunc(t,y):
             t = torch.tensor(t).to(y0.device, y0.dtype)
             y = torch.reshape(torch.tensor(y).to(y0.device, y0.dtype), y0.shape)
             with torch.no_grad():
-                f = dtfunc(t, y)
+                f = self.dtfunc(t, y)
             return f.detach().cpu().numpy().reshape(-1)
-        def dyfunc(t,y,v):
-            def yfunc(y):
-                return self.func(t,y)
-            output, jvp = torch.autograd.functional.jvp(yfunc,y,v)
-            return jvp
+        
+        
         def np_dyfunc(t,y,v):
             t = torch.tensor(t).to(y0.device, y0.dtype)
             y = torch.reshape(torch.tensor(y).to(y0.device, y0.dtype), y0.shape)
             v = torch.reshape(torch.tensor(v).to(y0.device, y0.dtype), y0.shape)
             with torch.no_grad():
-                f = dyfunc(t, y, v)
+                f = self.dyfunc(t, y, v)
             return f.detach().cpu().numpy().reshape(-1)
+        
         def integrand(s):
             value=np.zeros(len(s))
             for i in range(len(s)):
@@ -87,30 +85,25 @@ class AdaptiveCRSolver(AdaptiveGridODESolver):
         y1_np=y1[range(len(y0)-num_paras)].detach().cpu().numpy().reshape(-1)
         t0_np=t0.detach().cpu().numpy().reshape(-1)
         t1_np=t1.detach().cpu().numpy().reshape(-1)
-        def dtfunc(t,y):
-            yfull=torch.cat((y,torch.zeros(num_paras)))
-            def tfunc(t):
-                return self.func(t,yfull)[range(len(y0)-num_paras)]
-            return torch.autograd.functional.jacobian(tfunc,t)
+
         def np_dtfunc(t,y):
             y = torch.reshape(torch.tensor(y).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape)
+            yfull=torch.cat((y,torch.zeros(num_paras)))
             t = torch.tensor(t).to(y0.device, y0.dtype)
             with torch.no_grad():
-                f = dtfunc(t, y)
+                f = self.dtfunc(t, yfull)
             return f.detach().cpu().numpy().reshape(-1)
-        def dyfunc(t,y,v):
-            def yfunc(y):
-                yfull=torch.cat((y,torch.zeros(num_paras)))
-                return self.func(t,yfull)[range(len(y0)-num_paras)]
-            output, jvp = torch.autograd.functional.jvp(yfunc,y,v)
-            return jvp
+        
         def np_dyfunc(t,y,v):
             t = torch.tensor(t).to(y0.device, y0.dtype)
             y = torch.reshape(torch.tensor(y).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape)
+            yfull=torch.cat((y,torch.zeros(num_paras)))
             v = torch.reshape(torch.tensor(v).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape)
+            vfull=torch.cat((v,torch.zeros(num_paras)))
             with torch.no_grad():
-                f = dyfunc(t, y, v)
+                f = self.dyfunc(t, yfull, vfull)
             return f.detach().cpu().numpy().reshape(-1)
+        
         def integrand(s):
             value=np.zeros(len(s))
             for i in range(len(s)):

torchdiffeq/_impl/adaptiveLobatto.py
@@ -26,7 +26,7 @@ class AdaptiveLobattoSolver(AdaptiveGridODESolver):
            f2 = np_func(t1_np,y0_np+dt*(4/24* k0+16/24*k1+4/24*k2))
            f = np.concatenate((f1,f2))
            return k-f
-        k=sci.optimize.root(optim_f,[np_func(t0_np,y0_np),np_func(t0_np,y0_np)])
+        k=sci.optimize.root(optim_f,[np_func(t0_np,y0_np),np_func(t0_np,y0_np)],options={'xtol':1e-10})
         y1=y0_np+dt*(4/24* np_func(t0_np,y0_np)+16/24*k["x"][0:len(y0_np)]+4/24*k["x"][len(y0_np):2*len(y0_np)])
         self.yhalf=y0_np+dt*(5/24* np_func(t0_np,y0_np)+8/24*k["x"][0:len(y0_np)]-1/24*k["x"][len(y0_np):2*len(y0_np)])
         tr=torch.reshape(torch.tensor(y1).to(y0.device, y0.dtype), y0.shape)
@@ -53,19 +53,19 @@ class AdaptiveLobattoSolver(AdaptiveGridODESolver):
            f2 = np_func(t1_np,y0_np+dt*(4/24* k0+16/24*k1+4/24*k2))
            f = np.concatenate((f1,f2))
            return k-f
-        k=sci.optimize.root(optim_f,[np_func(t0_np,y0_np),np_func(t0_np,y0_np)])
+        k=sci.optimize.root(optim_f,[np_func(t0_np,y0_np),np_func(t0_np,y0_np)],options={'xtol':1e-10})
         y1=y0_np+dt*(4/24* np_func(t0_np,y0_np)+16/24*k["x"][0:len(y0_np)]+4/24*k["x"][len(y0_np):2*len(y0_np)])
         tr=torch.reshape(torch.tensor(y1).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape)
-        self.yhalf=y0_np+dt*(5/24* np_func(t0_np,y0_np)+8/24*k["x"][0:len(y0_np)]-1/24*k["x"][len(y0_np):2*len(y0_np)])
+        yhalf=y0_np+dt*(5/24* np_func(t0_np,y0_np)+8/24*k["x"][0:len(y0_np)]-1/24*k["x"][len(y0_np):2*len(y0_np)])
         #restlichen variablen:
         frest0=func(t0,y0)[range(-num_paras,0)]
-        fresthalf=func(t0+0.5*(t0-t1),torch.cat((torch.reshape(torch.tensor(self.yhalf).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape),torch.zeros(num_paras))))[range(-num_paras,0)]
+        fresthalf=func(t0+0.5*(t1-t0),torch.cat((torch.reshape(torch.tensor(yhalf).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape),torch.zeros(num_paras))))[range(-num_paras,0)]
         frest1=func(t1,torch.cat((tr,torch.zeros(num_paras))))[range(-num_paras,0)]
         rest=y0[range(-num_paras,0)]+torch.tensor(dt,dtype=y0.dtype)*(4/24* frest0+16/24*fresthalf+4/24*frest1)
-       
+        rest_half=y0[range(-num_paras,0)]+torch.tensor(dt,dtype=y0.dtype)*(5/24* frest0+8/24*fresthalf-1/24*frest1)
         
-        fresthalf_np=fresthalf.detach().cpu().numpy().reshape(-1)
-        self.yhalf=np.concatenate((self.yhalf,fresthalf_np))
+        rest_half=rest_half.detach().cpu().numpy().reshape(-1)
+        self.yhalf=np.concatenate((yhalf,rest_half))
         result=torch.cat((tr,rest))
         return result, func(t0,y0)
     
@@ -75,27 +75,20 @@ class AdaptiveLobattoSolver(AdaptiveGridODESolver):
         t0_np=t0.detach().cpu().numpy().reshape(-1)
         t1_np=t1.detach().cpu().numpy().reshape(-1)
         thalf=(t0_np+t1_np)/2.
-        def dtfunc(t,y):
-            def tfunc(t):
-                return self.func(t,y)
-            return torch.autograd.functional.jacobian(tfunc,t)
+        
         def np_dtfunc(t,y):
             t = torch.tensor(t).to(y0.device, y0.dtype)
             y = torch.reshape(torch.tensor(y).to(y0.device, y0.dtype), y0.shape)
             with torch.no_grad():
-                f = dtfunc(t, y)
+                f = self.dtfunc(t, y)
             return f.detach().cpu().numpy().reshape(-1)
-        def dyfunc(t,y,v):
-            def yfunc(y):
-                return self.func(t,y)
-            output, jvp = torch.autograd.functional.jvp(yfunc,y,v)
-            return jvp
+        
         def np_dyfunc(t,y,v):
             t = torch.tensor(t).to(y0.device, y0.dtype)
             y = torch.reshape(torch.tensor(y).to(y0.device, y0.dtype), y0.shape)
             v = torch.reshape(torch.tensor(v).to(y0.device, y0.dtype), y0.shape)
             with torch.no_grad():
-                f = dyfunc(t, y, v)
+                f = self.dyfunc(t, y, v)
             return f.detach().cpu().numpy().reshape(-1)
         def interpolant(t):
             return y0_np*(t-thalf)*(t-t1_np)/((t0_np-thalf)*(t0_np-t1_np))+self.yhalf*(t-t0_np)*(t-t1_np)/((thalf-t0_np)*(thalf-t1_np))+y1_np*(t-t0_np)*(t-thalf)/((t1_np-t0_np)*(t1_np-thalf))
@@ -115,40 +108,34 @@ class AdaptiveLobattoSolver(AdaptiveGridODESolver):
         num_paras=sum(p.numel() for p in self.adjoint_params)
         y0_np=y0[range(len(y0)-num_paras)].detach().cpu().numpy().reshape(-1)
         y1_np=y1[range(len(y0)-num_paras)].detach().cpu().numpy().reshape(-1)
-        self.yhalf=self.yhalf[range(len(y0)-num_paras)]
+        yhalf=self.yhalf[range(len(y0)-num_paras)]
         t0_np=t0.detach().cpu().numpy().reshape(-1)
         t1_np=t1.detach().cpu().numpy().reshape(-1)
         thalf=(t0_np+t1_np)/2.
-        def dtfunc(t,y):
-            yfull=torch.cat((y,torch.zeros(num_paras)))
-            def tfunc(t):
-                return self.func(t,yfull)[range(len(y0)-num_paras)]
-            return torch.autograd.functional.jacobian(tfunc,t)
+
         def np_dtfunc(t,y):
             y = torch.reshape(torch.tensor(y).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape)
+            yfull=torch.cat((y,torch.zeros(num_paras)))
             t = torch.tensor(t).to(y0.device, y0.dtype)
             with torch.no_grad():
-                f = dtfunc(t, y)
+                f = self.dtfunc(t, yfull)
             return f.detach().cpu().numpy().reshape(-1)
-        def dyfunc(t,y,v):
-            def yfunc(y):
-                yfull=torch.cat((y,torch.zeros(num_paras)))
-                return self.func(t,yfull)[range(len(y0)-num_paras)]
-            output, jvp = torch.autograd.functional.jvp(yfunc,y,v)
-            return jvp
+        
         def np_dyfunc(t,y,v):
             t = torch.tensor(t).to(y0.device, y0.dtype)
             y = torch.reshape(torch.tensor(y).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape)
+            yfull=torch.cat((y,torch.zeros(num_paras)))
             v = torch.reshape(torch.tensor(v).to(y0.device, y0.dtype), y0[range(len(y0)-num_paras)].shape)
+            vfull=torch.cat((v,torch.zeros(num_paras)))
             with torch.no_grad():
-                f = dyfunc(t, y, v)
+                f = self.dyfunc(t, yfull, vfull)
             return f.detach().cpu().numpy().reshape(-1)
         def interpolant(t):
-            return y0_np*(t-thalf)*(t-t1_np)/((t0_np-thalf)*(t0_np-t1_np))+self.yhalf*(t-t0_np)*(t-t1_np)/((thalf-t0_np)*(thalf-t1_np))+y1_np*(t-t0_np)*(t-thalf)/((t1_np-t0_np)*(t1_np-thalf))
+            return y0_np*(t-thalf)*(t-t1_np)/((t0_np-thalf)*(t0_np-t1_np))+yhalf*(t-t0_np)*(t-t1_np)/((thalf-t0_np)*(thalf-t1_np))+y1_np*(t-t0_np)*(t-thalf)/((t1_np-t0_np)*(t1_np-thalf))
         def dt_interpolant(t):
-            return y0_np*((t-thalf)+(t-t1_np))/((t0_np-thalf)*(t0_np-t1_np))+self.yhalf*((t-t0_np)+(t-t1_np))/((thalf-t0_np)*(thalf-t1_np))+y1_np*((t-t0_np)+(t-thalf))/((t1_np-t0_np)*(t1_np-thalf))
+            return y0_np*((t-thalf)+(t-t1_np))/((t0_np-thalf)*(t0_np-t1_np))+yhalf*((t-t0_np)+(t-t1_np))/((thalf-t0_np)*(thalf-t1_np))+y1_np*((t-t0_np)+(t-thalf))/((t1_np-t0_np)*(t1_np-thalf))
         def d2t_interpolant(t):
-            return y0_np*2./((t0_np-thalf)*(t0_np-t1_np))+self.yhalf*2./((thalf-t0_np)*(thalf-t1_np))+y1_np*2./((t1_np-t0_np)*(t1_np-thalf))
+            return y0_np*2./((t0_np-thalf)*(t0_np-t1_np))+yhalf*2./((thalf-t0_np)*(thalf-t1_np))+y1_np*2./((t1_np-t0_np)*(t1_np-thalf))
         def integrand(s):
             value=np.zeros(len(s))
             for i in range(len(s)):

torchdiffeq/_impl/adjoint.py
@@ -63,7 +63,9 @@ class OdeintAdjointMethod(torch.autograd.Function):
             # [-1] because y and grad_y are both of shape (len(t), *y0.shape)
             aug_state = [torch.zeros((), dtype=y.dtype, device=y.device), y[-1], grad_y[-1]]  # vjp_t, y, vjp_y
             aug_state.extend([torch.zeros_like(param) for param in adjoint_params])  # vjp_params
-
+            #shapes of adjoint for second derivatives:
+            shapes = [y0_.shape for y0_ in tuple(aug_state)]
+            adjoint_options['shapes'] = shapes
             ##################################
             #    Set up backward ODE func    #
             ##################################
@@ -93,7 +95,7 @@ class OdeintAdjointMethod(torch.autograd.Function):
 
                     vjp_t, vjp_y, *vjp_params = torch.autograd.grad(
                         func_eval, (t, y) + adjoint_params, -adj_y,
-                        allow_unused=True, retain_graph=True
+                        allow_unused=True, retain_graph=True,create_graph=True
                     )
 
                 # autograd.grad returns None if no gradient, set to zero.
@@ -117,6 +119,7 @@ class OdeintAdjointMethod(torch.autograd.Function):
             #       Solve adjoint ODE        #
             ##################################
 
+    
             if t_requires_grad:
                 time_vjps = torch.empty(len(t), dtype=t.dtype, device=t.device)
             else:
@@ -131,6 +134,8 @@ class OdeintAdjointMethod(torch.autograd.Function):
                     time_vjps[i] = dLd_cur_t
 
                 # Run the augmented system backwards in time.
+
+                
                 aug_state = odeint(
                     augmented_dynamics, tuple(aug_state),
                     t[i - 1:i + 1].flip(0),
@@ -203,9 +208,12 @@ def odeint_adjoint(func, y0, t, *, rtol=1e-7, atol=1e-9, method=None,faster_adj_
                           "excluded from the adjoint pass, and will not appear as a tensor in the adjoint norm.")
     if faster_adj_solve:
         adjoint_options['adjoint_params']=adjoint_params
+    
     # Convert to flattened state.
     shapes, func, y0, t, rtol, atol, method, options, event_fn, decreasing_time = _check_inputs(func, y0, t, rtol, atol, method, options, event_fn, SOLVERS)
 
+    adjoint_options['original_func']=func
+    adjoint_options['t_requires_grad'] = t.requires_grad
     # Handle the adjoint norm function.
     state_norm = options["norm"]
     handle_adjoint_norm_(adjoint_options, shapes, state_norm)

torchdiffeq/_impl/solvers.py
@@ -1,8 +1,9 @@
 import abc
 import torch
 from .event_handling import find_event
-from .misc import _handle_unused_kwargs
+from .misc import _handle_unused_kwargs, _TupleFunc, _ReverseFunc
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 
 class AdaptiveStepsizeODESolver(metaclass=abc.ABCMeta):
@@ -183,7 +184,7 @@ class FixedGridODESolver(metaclass=abc.ABCMeta):
 
 class AdaptiveGridODESolver(FixedGridODESolver):
 
-    def __init__(self, func, y0, atol, step_size, theta, interp="linear", perturb=False, faster_adj_solve=False,adjoint_params=None, conv_ana=False,file_id="None", **unused_kwargs):
+    def __init__(self, func, y0, atol, step_size, theta, interp="linear", perturb=False, faster_adj_solve=False,adjoint_params=None, conv_ana=False,file_id="None",max_nodes=1000,original_func=None,t_requires_grad=False,shapes=None, **unused_kwargs):
         self.atol = atol
         unused_kwargs.pop('rtol', None)
         unused_kwargs.pop('norm', None)
@@ -203,6 +204,84 @@ class AdaptiveGridODESolver(FixedGridODESolver):
         self.file_id=file_id
         self.faster_adj_solve=faster_adj_solve
         self.adjoint_params=adjoint_params
+        self.max_nodes=max_nodes
+        self.yhalf=None
+        if original_func is not None:
+            self.original_func=_ReverseFunc(original_func, mul=-1.0)
+            def augmented_dynamics_temp(t, y_aug):
+                # Dynamics of the original system augmented with
+                # the adjoint wrt y, and an integrator wrt t and args.
+                y = y_aug[1]
+                adj_y = y_aug[2]
+                
+
+                with torch.enable_grad():
+
+                   
+                    func_eval = self.original_func(t, y)
+
+                    # Workaround for PyTorch bug #39784
+                    _t = torch.as_strided(t, (), ())  # noqa
+                    _y = torch.as_strided(y, (), ())  # noqa
+                    _params = tuple(torch.as_strided(param, (), ()) for param in tuple(adjoint_params))  # noqa
+
+                    vjp_t, vjp_y, *vjp_params = torch.autograd.grad(
+                        func_eval, (t, y) + tuple(adjoint_params), -adj_y,
+                        allow_unused=True, retain_graph=True,create_graph=True
+                    )
+
+                # autograd.grad returns None if no gradient, set to zero.
+                vjp_t = torch.zeros_like(t) if vjp_t is None else vjp_t
+                vjp_t = torch.zeros_like(t) if not t_requires_grad else vjp_t
+                vjp_y = torch.zeros_like(y) if vjp_y is None else vjp_y
+                vjp_params = [torch.zeros_like(param) if vjp_param is None else vjp_param
+                              for param, vjp_param in zip(tuple(adjoint_params), vjp_params)]
+
+                return (vjp_t, func_eval, vjp_y, *vjp_params)
+            augmented_dynamics_temp=_TupleFunc(augmented_dynamics_temp,shapes)
+
+            if self.faster_adj_solve:
+                num_paras=sum(p.numel() for p in self.adjoint_params)
+                def augmented_dynamics_temp2(t, y_aug):
+                    return augmented_dynamics_temp(t,y_aug)[range(len(self.y0)-num_paras)]
+            else:
+                augmented_dynamics_temp2=augmented_dynamics_temp
+            def dtfunc(t,y):
+                t = t.detach().requires_grad_(True)
+                y = y.detach().requires_grad_(True)
+                def tfunc(t):
+                    return augmented_dynamics_temp2(t,y)
+                return torch.autograd.functional.jacobian(tfunc,t)
+            self.dtfunc=dtfunc
+
+            def dyfunc(t,y,v):
+                t = t.detach().requires_grad_(True)
+                y = y.detach().requires_grad_(True)
+                v = v.detach().requires_grad_(True)
+                def yfunc(y):
+                    return augmented_dynamics_temp2(t,y)
+                output, jvp = torch.autograd.functional.jvp(yfunc,y,v)
+                return jvp
+            self.dyfunc=dyfunc
+        else:
+            def dtfunc(t,y):
+                t = t.detach().requires_grad_(True)
+                y = y.detach().requires_grad_(True)
+                def tfunc(t):
+                    return self.func(t,y)
+                return torch.autograd.functional.jacobian(tfunc,t)
+            self.dtfunc=dtfunc
+
+            def dyfunc(t,y,v):
+                t = t.detach().requires_grad_(True)
+                y = y.detach().requires_grad_(True)
+                v = v.detach().requires_grad_(True)
+                def yfunc(y):
+                    return self.func(t,y)
+                output, jvp = torch.autograd.functional.jvp(yfunc,y,v)
+                return jvp
+            self.dyfunc=dyfunc
+        
 
     @abc.abstractmethod
     def _step_func_adjoint(self, func, t0, dt, t1, y0):
@@ -238,15 +317,20 @@ class AdaptiveGridODESolver(FixedGridODESolver):
         if self.conv_ana:
             len_grids=torch.tensor([time_grid.size(0)])
             estis_per=torch.tensor([torch.inf])
+            grids=time_grid.detach().clone()
         estis = torch.ones(time_grid.size(),dtype=self.y0.dtype,device=self.y0.device)*torch.inf
-        solution = torch.empty(len(t), *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device)
-        solution[0] = self.y0
 
-        while(torch.sum(estis)>self.atol):
-            estis = torch.zeros(time_grid.size(0)-1)
-            #if self.conv_ana:
-                #sol_grid=torch.empty(len(time_grid), *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device)
-                #sol_grid[0]=self.y0
+        while(torch.sum(estis)>self.atol and time_grid.size(0)<self.max_nodes):
+            solution = torch.empty(len(t), *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device)
+            solution[0] = self.y0
+            estis = torch.zeros(time_grid.size(0)-1,dtype=self.y0.dtype)
+            if self.conv_ana:
+                if self.yhalf is not None:
+                    sol_grid=torch.empty(2*len(time_grid)-1, *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device)
+                    sol_grid[0]=self.y0
+                else:
+                    sol_grid=torch.empty(len(time_grid), *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device)
+                    sol_grid[0]=self.y0
             assert time_grid[0] == t[0] and time_grid[-1] == t[-1]
 
             j = 1
@@ -262,8 +346,12 @@ class AdaptiveGridODESolver(FixedGridODESolver):
                 else:
                     y1, f0 = self._step_func(self.func, t0, dt, t1, y0)
                     estis[i] = self.eval_estimator(t0 , dt , t1, y0, y1)
-                #if self.conv_ana:    
-                    #sol_grid[i+1]=y1
+                if self.conv_ana:
+                    if self.yhalf is not None:
+                        sol_grid[2*i+1]=torch.reshape(torch.tensor(self.yhalf).to(y0.device, y0.dtype), y0.shape)
+                        sol_grid[2*i+2]=y1
+                    else:    
+                        sol_grid[i+1]=y1
                 i=i+1
                 while j < len(t) and t1 >= t[j] and t0 < t[j]:
                     if self.interp == "linear":
@@ -277,19 +365,26 @@ class AdaptiveGridODESolver(FixedGridODESolver):
                 y0 = y1
             time_grid=self.refine_grid(self,time_grid,estis)
             if self.conv_ana:
+                plt.step(time_grid[:-1],time_grid[1:]-time_grid[:-1],where="post")
+                plt.yscale("log")
+                plt.pause(0.05)
+                plt.clf()
+                print(sum(estis))
                 len_grids=torch.cat((len_grids,torch.tensor([time_grid.size(0)])))
+                grids=torch.cat((grids,time_grid.detach().clone()))
                 if torch.max(estis_per)<torch.inf:
                     estis_per=torch.cat((estis_per,torch.tensor([torch.sum(estis)])))
-                    sols.append(solution.detach().clone())
+                    sols.append(sol_grid.detach().clone())
                 else:
                     estis_per=torch.tensor([torch.sum(estis)])
-                    sols=[solution.detach().clone()]
+                    sols=[sol_grid.detach().clone()]
         if self.conv_ana:
             Path("results/").mkdir(parents=True, exist_ok=True)
             sols=torch.cat(sols,dim=0)
             torch.save(len_grids,"results/len_grids_"+self.file_id+".pt")
             torch.save(estis_per,"results/estis_"+self.file_id+".pt")
             torch.save(sols,"results/sols_"+self.file_id+".pt")
+            torch.save(grids,"results/grids_"+self.file_id+".pt")
             return solution
         else:
             return solution